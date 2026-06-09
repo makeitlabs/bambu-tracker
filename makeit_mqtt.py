e05e8cd514401f17787afef938f752f803fc2d73
robojay <jay@reactivetechnologies.com>
2026-06-09 15:02:07 -0400

    Adds MakeIt specific mqtt publishing

---
+++ makeit_mqtt.py
@@ -0,0 +1,64 @@
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import datetime
+
+class MakeItMQTT:
+        # --- Configuration ---
+        BROKER = "mqtt"
+        PORT = 8883  # Standard port for MQTT over TLS
+        TOPIC = "#"
+        client = None
+
+        # File paths to your certificates
+        # These should NEVER be added to the repository!!!
+        CA_CERT = "ca.crt"
+        CLIENT_CERT = "client_printmon.crt"
+        CLIENT_KEY = "client_printmon.key"
+
+        # --- Callback Functions ---
+        def on_connect(self,client, userdata, flags, rc, properties=None):
+                if rc == 0:
+                        pass
+                        #client.subscribe(TOPIC)
+                else:
+                        print(f"Connection failed with code {rc}")
+
+        def on_message(client, userdata, msg):
+                print(f"Topic: {msg.topic} | Message: {msg.payload.decode()}")
+
+        def update_printer_status(self,name,status,percent,remaining,job):
+                stat = json.dumps({
+                        "status":status,
+                        "percent":percent,
+                        "min_remaining":remaining,
+                        "reported":datetime.datetime.now().isoformat(),
+                        "job":job
+                        })
+                r = self.client.publish(f"printers/{name}",payload=stat,qos=1,retain=1)
+                r.wait_for_publish()
+
+        def __init__(self):
+                self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+
+
+                self.client.on_connect = self.on_connect
+                #self.client.on_message = self.on_message
+
+                self.client.tls_set(
+                        ca_certs=self.CA_CERT,
+                        certfile=self.CLIENT_CERT,
+                        keyfile=self.CLIENT_KEY,
+                        tls_version=ssl.PROTOCOL_TLSv1_2
+                )
+                self.client.connect(self.BROKER, self.PORT, keepalive=60)
+                self.client.loop_start()
+
+if __name__ == "__main__":
+        test = MakeItMQTT()
+        test.update_printer_status("test","test",20,10,"myprint")
+
+        # 5. Clean up and stop the background thread
+        test.client.disconnect()
+        test.client.loop_stop()
+        
